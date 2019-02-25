60418be8f7979a87338dd3e2a502c53a8cde99ff
rishiraj <rasraj330@gmail.com>
2019-02-25 23:50:04 +0530

    first commit

---
+++ flaskblog/config.py
@@ -0,0 +1,8 @@
+class Config:
+    SECRET_KEY = '892e56dd747570b10f3a198e27fe5526'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '2017ugcs020@nitjsr.ac.in'
+    MAIL_PASSWORD = 'Rishi@1998'
